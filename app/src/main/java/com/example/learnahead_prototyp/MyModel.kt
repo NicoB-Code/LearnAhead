ba6a44686a9fcee1a18d5b7c9a36d0f45e27c0cc
NicoB-Code <82388053+NicoB-Code@users.noreply.github.com>
2023-03-29 18:28:05 +0200

    Database connection now stable - login prototype now working

--- app/src/main/java/com/example/learnahead_prototyp/MyModel.kt
+++ app/src/main/java/com/example/learnahead_prototyp/MyModel.kt
@@ -1,5 +1,36 @@
 package com.example.learnahead_prototyp
 
+import android.util.Log
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
 class MyModel {
     var myData: String = "Hello, MVVM!"
+    var dbConnection = Firebase.firestore
+
+    fun checkUserCredentials(username: String, password: String, onComplete: (Boolean) -> Unit) {
+        // Get a reference to the "users" collection in Firestore
+        val usersCollection = FirebaseFirestore.getInstance().collection("users")
+
+        // Query the "users" collection for a document with the specified username and password
+        usersCollection
+            .whereEqualTo("username", username)
+            .whereEqualTo("password", password)
+            .get()
+            .addOnSuccessListener { querySnapshot ->
+                // If a document was found with the specified username and password, return true
+                if (!querySnapshot.isEmpty) {
+                    onComplete(true)
+                } else {
+                    onComplete(false)
+                }
+            }
+            .addOnFailureListener { exception ->
+                // Handle any errors that occurred while querying the Firestore database
+                Log.e("Firestore", "Error checking user credentials", exception)
+                onComplete(false)
+            }
+    }
+
 }
